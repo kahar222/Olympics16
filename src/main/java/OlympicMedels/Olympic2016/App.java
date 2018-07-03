4e2b1604fa503852879466f2cad579aea48d4bdc
Kens <afrayin903@gmail.com>
2018-07-03 14:50:12 -0400

    add file

---
+++ src/main/java/OlympicMedels/Olympic2016/App.java
@@ -0,0 +1,13 @@
+package OlympicMedels.Olympic2016;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
